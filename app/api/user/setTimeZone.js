eae6ac9a8feca38ac017df54afb93d575db9cb6f
EugeneAZDev <eugene.zolotkov@gmail.com>
2023-08-17 21:19:43 +0300

    Split migrations files from .js

--- app/api/user/setTimeZone.js
+++ app/api/user/setTimeZone.js
@@ -1,6 +1,7 @@
 ({
   method: async ({ clientId, timeZone }) => {
     try {
+      console.log(`DEBUG INFO: ClientId: ${clientId}, timeZone: ${timeZone}`);
       await crud('User').update({ id: clientId, fields: { timeZone } });
       common.userTimeZoneMap.set(clientId, timeZone);
       return responseType.success();
