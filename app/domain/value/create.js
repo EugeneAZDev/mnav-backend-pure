d1797ce8a5daba62c041e88d58c3bca238b48969
EugeneAZDev <eugene.zolotkov@gmail.com>
2023-10-23 10:18:18 +0300

    Bug fixes in create value method

--- app/domain/value/create.js
+++ app/domain/value/create.js
@@ -1,19 +1,18 @@
 async (pool, clientId, records) => {
+  const { itemId, value } = records;
+  const itemInfo = { itemId };
+  if (typeof value === 'string') itemInfo['title'] = value;
   const valueDetails = await crud('ValueDetail').select({
-    where: { itemId: records.itemId },
+    where: itemInfo,
     transaction: pool,
   });
 
   const inputDate = records.createdAt && new Date(records.createdAt);
   const localDate = await domain.getLocalTime(clientId, inputDate);
   const createdAt = localDate || (await domain.getLocalTime(clientId));
-  const result = await crud('ItemValue').create(
-    [{ ...records, createdAt }],
-    pool,
-  );
-  const [value] = result.rows;
-  const { itemId } = value;
-  const details = { itemId, latestAt: createdAt };
+  await crud('ItemValue').create([{ ...records, createdAt }], pool);
+
+  const details = { ...itemInfo, latestAt: createdAt };
   if (valueDetails.rows.length) {
     const existingRec = valueDetails.rows[0];
     await crud('ValueDetail').update({
@@ -21,7 +20,9 @@ async (pool, clientId, records) => {
       fields: details,
       transaction: pool,
     });
-  } else await crud('ValueDetail').create([details], pool);
+  } else {
+    await crud('ValueDetail').create([details], pool);
+  }
 
   return value.id;
 };
