76807ebe7f697a8c624a88df6382895ec9185827
EugeneAZDev <eugene.zolotkov@gmail.com>
2023-12-07 20:19:31 +0200

    Add function to get titles for text type values

--- app/api/value/getTitles.js
+++ app/api/value/getTitles.js
@@ -2,27 +2,24 @@
   method: async ({ clientId, itemId, pool }) => {
     try {
       const sql = `
-        SELECT iv.value, iv."createdAt", i."valueType"
+        SELECT DISTINCT iv.value AS title
         FROM "ItemValue" iv
         JOIN "Item" i ON iv."itemId" = i.id
         LEFT JOIN "ItemSection" its ON its.id = i."sectionId" 
         WHERE	i."userId" = ${clientId}
           AND iv."itemId" = ${itemId}
           AND  iv."deletedAt" IS NULL
-        ORDER BY iv."createdAt";
+          AND i."valueType" = 'text'
       `;
       const result = await crud().query(sql, [], pool);
+      const titles = result.rows.map((item) => item.title);
       if (result.rows.length > 0) {
         return responseType.modifiedBodyTemplate(responseType.success, {
-          valueType: result.rows[0].valueType,
-          values: result.rows.map((row) => ({
-            value: row.value,
-            createdAt: row.createdAt,
-          })),
+          titles,
         });
       }
       return responseType.modifiedBodyTemplate(responseType.success, {
-        values: [],
+        titles: [],
       });
     } catch (error) {
       return { ...responseType.error(), error };
