7e9f7219e0cb3553e1c875077184dbf564e56740
EugeneAZDev <eugene.zolotkov@gmail.com>
2024-03-04 11:56:56 +0200

    change getHours to getUTCHours for getLocalTime

--- app/domain/getLocalTime.js
+++ app/domain/getLocalTime.js
@@ -1,4 +1,4 @@
-async (clientId, time) => {
+async (clientId, time) => {  
   let tz = common.userTimeZoneMap.get(clientId);
   if (!tz) {
     tz = (
@@ -11,18 +11,16 @@ async (clientId, time) => {
     common.userTimeZoneMap.set(clientId, tz);
   }
 
-  const originDate = new Date(time);  
-  const summerTZ = new Date(originDate.getFullYear(), 7, 1).getTimezoneOffset();
-
-  const offset = originDate.getTimezoneOffset();
-  const originHours = originDate.getHours();
-  const originMinutes = originDate.getMinutes();
-
-  const date = (time && new Date(time)) || new Date();
+  const date = (time && new Date(time)) || new Date();  
+  const offset = date.getTimezoneOffset();
+  const originHours = date.getUTCHours();
+  const originMinutes = date.getMinutes();
+  const summerTZ = new Date(date.getFullYear(), 7, 1).getTimezoneOffset();
+  
   let adjustedTimestamp = new Date(date.getTime() + tz * 60 * 60 * 1000);  
-  if (offset === summerTZ && originHours === 0 && originMinutes === 0) {
+  if (offset === summerTZ && originHours === 21 && originMinutes === 0) {
     adjustedTimestamp = new Date(adjustedTimestamp.getTime() + (1 * 60 * 60 * 1000));    
   }
-
+  
   return adjustedTimestamp;
 };
