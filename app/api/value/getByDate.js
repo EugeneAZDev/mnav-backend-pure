b3ac63302dd993827b02aaa539c6243cb1457dc2
EugeneAZDev <eugene.zolotkov@gmail.com>
2024-03-07 17:00:31 +0200

    Improve endpoint to get range date of values

--- app/api/value/getByDate.js
+++ app/api/value/getByDate.js
@@ -1,16 +1,25 @@
 ({
-  method: async ({ clientId, id, date }) => {
-    try {      
+  method: async ({ clientId, id, date, daysCount }) => {
+    try {
       const localTime = await domain.getLocalTime(clientId, date);      
       const localDate = new Date(localTime).toISOString().split('T')[0];
       const idCondition = id ? `AND i.id = ${id}` : '';
+      let datesCondition = `AND DATE(iv."createdAt") = '${localDate}'`;
+      let createdAt = '';
+      if (daysCount && daysCount > 1) {
+        const secondDateTime = new Date(date);
+        secondDateTime.setDate(secondDateTime.getDate() + daysCount);
+        const secondDate = secondDateTime.toISOString().slice(0, 10);
+        datesCondition = `AND DATE(iv."createdAt") >= '${localDate}' AND DATE(iv."createdAt") <= '${secondDate}'`
+        createdAt = `, iv."createdAt"`;
+      }      
       const sql = `
-        SELECT iv.id, value, iv."itemId"
+        SELECT iv.id, value, iv."itemId"${createdAt}
         FROM "ItemValue" iv
           JOIN "Item" i ON	iv."itemId" = i.id
         WHERE	i."userId" = ${clientId}
           ${idCondition}
-          AND DATE(iv."createdAt") = '${localDate}'
+          ${datesCondition}
           AND iv."deletedAt" IS NULL;
         `;
       const result = await crud().query(sql);
