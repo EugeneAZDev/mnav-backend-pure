f5de5a57c895f13354e4130ceb1cb545ea4bbe56
EugeneAZDev <eugene.zolotkov@gmail.com>
2024-03-15 15:56:08 +0200

    Fix statistics for text values during update

---
+++ app/domain/value/update.js
@@ -0,0 +1,33 @@
+async (pool, clientId, records) => {
+  const { id, ...data } = records;
+  const updatedAt = await domain.getLocalTime(clientId);  
+
+  const valueInfo = await crud('ItemValue').select({ id, transaction: pool });
+  if (valueInfo && valueInfo.rows.length > 0) {
+    const { itemId, value: previousValue } = valueInfo.rows[0];
+    const itemInfo = await crud('Item').select({
+      id: itemId,
+      transaction: pool,
+    });
+    if (itemInfo && itemInfo.rows[0].valueType === 'text') {
+      const detailRecordResult = await crud('ValueDetail').select({
+        where: { title: previousValue },
+        transaction: pool,
+      });
+      if (detailRecordResult && detailRecordResult.rows.length > 0) {
+        const { id: detailsId } = detailRecordResult.rows[0];
+        await crud('ValueDetail').update({
+          id: parseInt(detailsId),
+          fields: { deletedAt: updatedAt },
+          transaction: pool,
+        });
+      }
+    }
+  };
+
+  await crud('ItemValue').update({
+    id,
+    fields: { updatedAt, ...data },
+    transaction: pool,
+  });
+};
