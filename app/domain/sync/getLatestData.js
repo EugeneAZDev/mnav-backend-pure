31aab8cebc05c12f0e0ef0236a15ff26d7b1b5d4
EugeneAZDev <eugene.zolotkov@gmail.com>
2024-05-28 12:59:53 +0300

    Update logic for the latest data of sync

--- app/domain/sync/getLatestData.js
+++ app/domain/sync/getLatestData.js
@@ -1,21 +1,37 @@
 async (pool, clientId, tableName, localDates) => {
   const getLatestDates = async (param) => {
-    const query = `
+    const basicQuery = `
       SELECT "${param}At" FROM "${tableName}" s
       WHERE s."userId" = ${clientId} AND s."${param}At" IS NOT NULL
       ORDER BY s."${param}At" DESC LIMIT 1;
     `;
+    const itemValueQuery = `
+      SELECT iv."${param}At" FROM "${tableName}" iv
+        JOIN "Item" i ON iv."itemId" = i.id
+        LEFT JOIN "ItemSection" its ON its.id = i."sectionId" 
+      WHERE	i."userId" = ${clientId}
+      ORDER BY iv."${param}At" DESC LIMIT 1;
+    `;
+    const query = tableName === 'ItemValue' ? itemValueQuery : basicQuery;
     const { [`${param}At`]: timestamp } = (await pool.query(query)).rows[0];
     return timestamp;
   };
 
-  const getData = async (param, latestTime, localTime) => {
-    const query = `
+  const getData = async (param, latestTime, localTime) => {    
+    const basicQuery = `
       SELECT * FROM "${tableName}" s
       WHERE s."userId" = ${clientId} AND s."${param}At" IS NOT NULL
         AND s."${param}At" > '${localTime}' AND s."${param}At" <= TIMESTAMP '${latestTime}' + INTERVAL '1 day'
       ORDER BY s."${param}At" DESC;
-    `;
+    `; 
+    const itemValueQuery = `
+      SELECT iv.* FROM "${tableName}" iv
+        JOIN "Item" i ON iv."itemId" = i.id
+        LEFT JOIN "ItemSection" its ON its.id = i."sectionId" 
+      WHERE	i."userId" = ${clientId} AND iv."deletedAt" IS NULL
+      ORDER BY 1, iv."createdAt"
+    `; // Getting only active values due to the large number
+    const query = tableName === 'ItemValue' ? itemValueQuery : basicQuery;
     const result = await pool.query(query);
     return result;
   };
@@ -28,26 +44,11 @@ async (pool, clientId, tableName, localDates) => {
     const latestDate = new Date(latestTime);
     const localDate = new Date(localTime);
 
-    // TODO REMOVE console.log('\n'); 
     if (latestDate.getTime() !== localDate.getTime()) {
-      // TODO REMOVE
-      // console.log(param);
-      // console.log('latestDate', latestDate);
-      // console.log('localDate', localDate);
-      // console.log('latestDateTime', latestDate.getTime());
-      // console.log('localDateTime', localDate.getTime());
-
       const result = await getData(param, latestTime, localTime);
       const ids = result.rows.map(item => item.id);
       return { ids, result: result.rows, latest };  
-    } else {
-      // TODO REMOVE
-      // console.log(param);
-      // console.log('latestDate', latestDate);
-      // console.log('localDate', localDate);
-      // console.log('Nothing to return\n\n');
-      return { ids: [], result: [] }
-    }
+    } else return { ids: [], result: [] }
   };
 
   const getResultByIds = (resultList, idArray) => resultList.filter(item => idArray.includes(item.id));
@@ -64,7 +65,7 @@ async (pool, clientId, tableName, localDates) => {
   const created = getResultByIds(preCreated.result, createdIds);
   const deleted = preDeleted.result;
 
-  const latestDateSet = {
+  const updatedLocalDates = {
     created: preCreated.latest,
     updated: preUpdated.latest,
     deleted: preDeleted.latest
@@ -74,6 +75,6 @@ async (pool, clientId, tableName, localDates) => {
     created,
     deleted,
     updated,
-    latestDateSet
+    updatedLocalDates
   }
 }
