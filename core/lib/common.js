078ac5835510f1347f5a435803ab7d9668739760
EugeneAZDev <eugene.zolotkov@gmail.com>
2024-09-30 10:31:03 +0300

    Remove resources

--- core/lib/common.js
+++ core/lib/common.js
@@ -246,7 +246,7 @@ const validateToken = (token) => {
 const getEmailContent = (contentPath, locale, type) => {
   const filePath = path.join(
     contentPath,
-    `./resource/email/${locale}/${type}.html`,
+    `./resources/email/${locale}/${type}.html`,
   );
   const content = fs.readFileSync(filePath, 'utf8');
 
