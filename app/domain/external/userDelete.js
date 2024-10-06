1965cc6b08ef224a18bd12e3e583dcab1a1031fe
EugeneAZDev <eugene.zolotkov@gmail.com>
2024-10-06 19:24:56 +0300

    Display to user only active deleted records

--- app/domain/external/userDelete.js
+++ app/domain/external/userDelete.js
@@ -6,7 +6,7 @@ async (pool, deletionToken) => {
   });  
   if (result.rows.length === 1) {
     const [ user ] = result.rows;
-    const locale = user.locale;
+    const locale = user.locale || 'en';    
     if (user) {
       const sent = await api.user.sendEmail().method({
         clientId: user.id,
@@ -14,7 +14,7 @@ async (pool, deletionToken) => {
         url: undefined,
         token: undefined,
         type: 'deleted',
-        inputLocale: user.locale,
+        inputLocale: locale,
       });
       if (sent.code === 200) {
         const destroyCountsResult = await api.user.destroy().method({ clientId: user.id });
--- app/domain/user/destroy.js
+++ app/domain/user/destroy.js
@@ -2,11 +2,32 @@ async (pool, clientId) => {
   const sqlDestroyDetails = 
     `DELETE FROM "ValueDetail" vd USING "Item" i WHERE vd."itemId" = i.id AND i."userId" = ${clientId};`;
   const destroyDetailsResult = await crud().query(sqlDestroyDetails, undefined, pool);
-  const sqlDestroyValues = 
+  const sqlDestroyDeletedValues = `
+    DELETE FROM "ItemValue" iv USING "Item" i
+    WHERE
+      iv."itemId" = i.id AND
+      iv."deletedAt" IS NOT NULL AND
+      i."userId" = ${clientId};`;
+  await crud().query(sqlDestroyDeletedValues, undefined, pool);
+  const sqlDestroyValues =
     `DELETE FROM "ItemValue" iv USING "Item" i WHERE iv."itemId" = i.id AND	i."userId" = ${clientId};`;
   const destroyValuesResult = await crud().query(sqlDestroyValues, undefined, pool);
+  const sqlDestroyDeletedItems = `
+    DELETE FROM "Item" i
+      WHERE
+        i."deletedAt" IS NOT NULL AND
+        i."userId" = ${clientId};
+  `;
+  await crud().query(sqlDestroyDeletedItems, undefined, pool);
   const sqlDestroyItems = `DELETE FROM "Item" i WHERE i."userId" = ${clientId};`;
   const destroyItemsResult = await crud().query(sqlDestroyItems, undefined, pool);
+  const sqlDestroyDeletedSections = `
+    DELETE FROM "ItemSection" is2
+    WHERE
+      is2."deletedAt" IS NOT NULL AND
+      is2."userId" = ${clientId}
+  `;
+  await crud().query(sqlDestroyDeletedSections, undefined, pool);
   const sqlDestroySections = `DELETE FROM "ItemSection" is2 WHERE is2."userId" = ${clientId}`;
   const destroySectionsResult = await crud().query(sqlDestroySections, undefined, pool);
   const sqlDestroyPayments = `DELETE FROM "Payment" p WHERE p."userId" = ${clientId}`;
